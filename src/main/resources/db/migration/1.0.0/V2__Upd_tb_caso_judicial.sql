395e0568318a7adf77975be4e40e8232bda004af
Carlos Romel Pereira da Silva <carlos.romel@gmail.com>
2024-04-01 02:35:08 -0300

    Ajustar o tamanho do número único e incluir a restrição da coluna cd_decisao

---
+++ src/main/resources/db/migration/1.0.0/V2__Upd_tb_caso_judicial.sql
@@ -0,0 +1,15 @@
+/**
+ * O número único do processo é persistido com sua máscara, passando a usar 25 caracteres.
+ */
+alter table curso2.tb_caso_judicial
+    alter column nr_numero_unico type varchar(25);
+
+alter table curso2.tb_caso_judicial
+    add constraint cd_decisao_check check (cd_decisao in ('A' -- Ativo
+                                                        , 'I' -- Inativo
+                                                        , 'C' -- Concluído
+                                                         ));
+
+comment on column curso2.tb_caso_judicial.cd_decisao is
+    'Código da decisão judicial(A: Ativo, I: Inativo ou C: Concluído).';
+
