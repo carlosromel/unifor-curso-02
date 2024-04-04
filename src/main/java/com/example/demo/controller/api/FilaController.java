0e7c9a80a823edc31ad411c32a8135a916ba09d0
Carlos Romel Pereira da Silva <carlos.romel@gmail.com>
2024-04-04 09:51:04 -0300

    Reposicionar as classes

---
+++ src/main/java/com/example/demo/controller/api/FilaController.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2024 Carlos Romel Pereira da Silva, <carlos.romel@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package com.example.demo.controller.api;
+
+import com.example.demo.rabbitproducer.Producer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Carlos Romel Pereira da Silva, <carlos.romel@gmail.com>
+ */
+@RestController
+@RequestMapping("/fila")
+public class FilaController {
+
+    @Autowired
+    private Producer producer;
+
+    @GetMapping
+    public String send() throws Exception {
+        producer.send("ENVIANDO ISSO!");
+
+        return "Enviado";
+    }
+}
